8111de871a1def32aecc438922577a4ad387a622
丁佳男 <jiananst@qq.com>
2025-06-03 18:47:23 +0800

    给线程安全问题的原因分析补充文字

--- projects/myThread/src/com/stone/threadSafe/case01/MyThread.java
+++ projects/myThread/src/com/stone/threadSafe/case01/MyThread.java
@@ -3,19 +3,19 @@ package com.stone.threadSafe.case01;
 public class MyThread extends Thread {
 
     // 表示这个类所有的对象，都共享ticketNum数据
-    static int ticketNum = 1;
+    static int ticket = 0;
 
     @Override
     public void run() {
         while (true) {
-            if (ticketNum <= 100) {
+            if (ticket < 100) {
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println(getName() + "正在出售第" + ticketNum + "张票");
-                ticketNum++;
+                ticket++;
+                System.out.println(getName() + "正在出售第" + ticket + "张票");
             } else break;
         }
     }
